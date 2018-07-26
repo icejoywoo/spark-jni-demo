9b9e2449a6e01c857627ee78a2f04abd0457ee41
icejoywoo <icejoywoo@gmail.com>
2018-07-26 11:13:51 +0800

    update

--- Base64.cpp
+++ Base64.cpp
@@ -12,10 +12,11 @@ JNIEXPORT jstring JNICALL Java_Base64_encode
     const char* data_ptr = env->GetStringUTFChars(data, 0);
     int output_size = Base64::EncodedLength(data_size);
 
-    char* buf = new char[output_size];
+    char* buf = new char[output_size + 1];
     if (!Base64::Encode(data_ptr, data_size, buf, output_size)) {
         printf("Failed to encode input string\n");
     }
+    buf[output_size] = '\0';
 
     jstring ret = env->NewStringUTF(buf);
     delete[] buf;
@@ -33,10 +34,11 @@ JNIEXPORT jstring JNICALL Java_Base64_decode
     const char* data_ptr = env->GetStringUTFChars(data, 0);
     int output_size = Base64::DecodedLength(data_ptr, data_size);
 
-    char* buf = new char[output_size];
+    char* buf = new char[output_size + 1];
     if (!Base64::Decode(data_ptr, data_size, buf, output_size)) {
         printf("Failed to decode input string\n");
     }
+    buf[output_size] = '\0';
 
     jstring ret = env->NewStringUTF(buf);
     delete[] buf;
